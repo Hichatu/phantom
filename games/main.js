a0a8a2e2167f372df75e1c8a43b888822948ef57
Hichatu <realhichatu@gmail.com>
2019-05-03 22:39:02 +0100

    what else do i put here

--- games/main.js
+++ games/main.js
@@ -1,21 +1,9 @@
 var gameData = {
-  clicks: 0
+  clicks: 0,
   clicksPerClick: 1
-  clicksPerClickCost: 10
 }
 
-function clickClicks()
-{
+function clickClicks() {
 	gameData.clicks += gameData.clicksPerClick
 	document.getElementById("clicksClicked").innerHTML = gameData.clicks + " Clicks Clicked"
-}
-
-function buyClicksPerClick()
-{
-	if (gameData.clicks >= gameData.clicksPerClickCost)
-	{
-		gameData.clicks -= gameData.clicksPerClickCost
-		gameData.clicksPerClick += 1
-		gameData.clicksPerClickCost *= 2
-	}
 }
