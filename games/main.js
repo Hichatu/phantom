cb54a387d2b2af337345f6c60645aec5e1e1b10d
Hichatu <47224085+Hichatu@users.noreply.github.com>
2019-05-03 22:44:01 +0100

    Delete main.js

--- games/main.js
+++
@@ -1,9 +0,0 @@
-var gameData = {
-  clicks: 0,
-  clicksPerClick: 1
-}
-
-function clickClicks() {
-	gameData.clicks += gameData.clicksPerClick
-	document.getElementById("clicksClicked").innerHTML = gameData.clicks + " Clicks Clicked"
-}
