0d59f5af6c56ddfc56341496b9c7de7c03a54224
Hichatu <realhichatu@gmail.com>
2019-05-04 16:04:21 +0100

    Added savegames

--- games/main.js
+++ games/main.js
@@ -7,3 +7,12 @@ function clickClicks() {
 	gameData.clicks += gameData.clicksPerClick
 	document.getElementById("clicksClicked").innerHTML = gameData.clicks + " Clicks Clicked"
 }
+
+var saveGameLoop = window.setInterval(function() {
+	localStorage.setItem("clickClickerSave", JSON.stringify(gameData))
+}, 1)
+
+var savegame = JSON.parse(localStorage.getItem("clickClickerSave"))
+if (savegame !== null) {
+	gameData = savegame
+}
