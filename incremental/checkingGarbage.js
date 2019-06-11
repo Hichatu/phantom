42e96fb9e38e10ca16970fe960eb4c24e05c1026
Hichatu <47224085+Hichatu@users.noreply.github.com>
2019-06-11 11:22:26 +0100

    remaking site

--- incremental/checkingGarbage.js
+++
@@ -1,6 +0,0 @@
-var checkAutoClickerLoop = window.setInterval(function() {
-	if (gameData.clicks >= 128) {
-	document.getElementById("AutoClickerButton").innerHTML = '<button onclick="buyAutoClicker()">Buy AutoClicker (Cost 256)</button>'
-	clearInterval(checkAutoClickerLoop)
-	}
-}, 1)
