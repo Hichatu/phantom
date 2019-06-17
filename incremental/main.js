c2314d5261e679c7532aa16a00f625bb83a4f8c4
Hichatu <realhichatu@gmail.com>
2019-06-17 20:11:41 +0100

    Actually Fixed Klicks

--- incremental/main.js
+++ incremental/main.js
@@ -3,17 +3,7 @@ var gameData = {
 	klicksPerClick: 1,
 }
 
-var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem("klickClickerSave", JSON.stringify(gameData))
-}, 1000)
-
-var savegame = JSON.parse(localStorage.getItem("klickClickerSave"))
-if (savegame !== null) {
-  gameData = savegame
-}
-
-
-function click() {
+function clickButton() {
 	gameData.klicks += gameData.klicksPerClick
 	document.getElementById("klicks").innerHTML = gameData.klicks
 }
---
+++ incremental/savegame.js
@@ -0,0 +1,8 @@
+var saveGameLoop = window.setInterval(function() {
+  localStorage.setItem("klickClickerSave", JSON.stringify(gameData))
+}, 1000)
+
+var savegame = JSON.parse(localStorage.getItem("klickClickerSave"))
+if (savegame !== null) {
+  gameData = savegame
+}
