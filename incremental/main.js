434fdf259d14d3a643973fd9e690ab28cd4d9989
Hichatu <realhichatu@gmail.com>
2019-06-17 19:44:11 +0100

    Redesigned Klicks

--- incremental/main.js
+++ incremental/main.js
@@ -1,9 +1,19 @@
 var gameData = {
-  klicks: 0,
-  klicksPerClick: 1
+	klicks: 0,
+	klicksPerClick: 1,
 }
 
+var saveGameLoop = window.setInterval(function() {
+  localStorage.setItem("klickClickerSave", JSON.stringify(gameData))
+}, 1000)
+
+var savegame = JSON.parse(localStorage.getItem("klickClickerSave"))
+if (savegame !== null) {
+  gameData = savegame
+}
+
+
 function click() {
-  gameData.klicks += gameData.klicksPerClick
-  document.getElementById("klicks").innerHTML = gameData.klicks
+	gameData.klicks += gameData.klicksPerClick
+	document.getElementById("klicks").innerHTML = gameData.klicks
 }
